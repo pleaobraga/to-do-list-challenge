a73aa20ec0fe7c4cd1cade99fba70e851417b3ee
AlexMeloFilho <100002081+AlexMeloFilho@users.noreply.github.com>
2022-02-28 11:49:12 -0300

    Update stack.js

--- stack.js
+++ stack.js
@@ -1,72 +1,74 @@
-const task = document.querySelector('#task')
-const addButton = document.querySelector('#add-button')
-const list = document.querySelector('#task-list')
-let taskList = JSON.parse(localStorage.getItem('taskList')) || []
+const task = document.querySelector('#task');
+const addButton = document.querySelector('#add-button');
+const list = document.querySelector('#task-list');
+let taskList = JSON.parse(localStorage.getItem('taskList')) || [];
+let justRender = false;
 
 const addTask = (element) => {
 
-  const listItem = document.createElement('li')
-  const sectionItem = document.createElement('div')
-  const statusItem = document.createElement('input')
+  const listItem = document.createElement('li');
+  const sectionItem = document.createElement('div');
+  const statusItem = document.createElement('input');
 
-  listItem.id = element.id 
+  listItem.id = element.id;
 
-  taskList.push(element)
-
-  sectionItem.textContent = element.name
-
-  statusItem.setAttribute('type', 'checkbox')
-
-  statusItem.addEventListener('input', (event) => {
-    const itemIndex = taskList.findIndex((item) => item.id === element.id)
-    taskList[itemIndex].done = !taskList[itemIndex].done
-    if (taskList[itemIndex].done) {
-      sectionItem.classList.add('line')
-    } else {
-      sectionItem.classList.remove('line')
-    }
-    localStorage.setItem('taskList', JSON.stringify(taskList));
-  } )
-
-  if (element.done) {
-    sectionItem.classList.add('line')
+  if (justRender === false) {
+    taskList.push(element);
   }
   
-  const deleteItem = document.createElement('button')
-  deleteItem.textContent = 'Deletar Tarefa'
+  sectionItem.textContent = element.name;
+
+  statusItem.setAttribute('type', 'checkbox');
+
+  statusItem.addEventListener('input', (event) => {
+    const itemIndex = taskList.findIndex((item) => item.id === element.id);
+    taskList[itemIndex].done = !taskList[itemIndex].done;
+    if (taskList[itemIndex].done) {
+      sectionItem.classList.add('line');
+    } else {
+      sectionItem.classList.remove('line');
+    }
+    localStorage.setItem('taskList', JSON.stringify(taskList));
+  });
+
+  if (element.done) {
+    sectionItem.classList.add('line');
+  }
+  
+  const deleteItem = document.createElement('button');
+  deleteItem.textContent = 'Deletar Tarefa';
 
   deleteItem.addEventListener('click', (event) => {
-    taskList = taskList.filter( (item) => item.id !== element.id )
-    listItem.remove()
+    taskList = taskList.filter( (item) => item.id !== element.id );
+    listItem.remove();
     localStorage.setItem('taskList', JSON.stringify(taskList));
   })
   
-  sectionItem.appendChild(statusItem)
-  sectionItem.appendChild(deleteItem)
+  sectionItem.appendChild(statusItem);
+  sectionItem.appendChild(deleteItem);
 
-  listItem.appendChild(sectionItem)
+  listItem.appendChild(sectionItem);
   
   list.appendChild(listItem)
- 
-}
+};
 
 taskList.forEach((task) => {
-  addTask(task)
+  justRender = true;
+  addTask(task);
 })
 
 
 addButton.addEventListener('click', (event) => {
-  const id = taskList.length > 0 ? parseInt(taskList[taskList.length -1 ].id) + 1 : 0
-  const newTask = {name: task.value, id, done: false}
-  
-  event.preventDefault()
+  const id = taskList.length > 0 ? parseInt(taskList[taskList.length -1 ].id) + 1 : 0;
+  const newTask = {name: task.value, id, done: false};
+  justRender = false;
+
+  event.preventDefault();
   
   setTimeout(() => {
-    addTask(newTask)
+    addTask(newTask);
     localStorage.setItem('taskList', JSON.stringify(taskList));
-  }, 2000)
+  }, 2000);
   
-  task.value = ''
-  
-})
-
+  task.value = '';
+});
